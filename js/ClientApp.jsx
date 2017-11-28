f6be83e85b5c3e88144040347f95c449e59a680f
thismax <max.g.jacobs@gmail.com>
2017-11-28 15:59:53 -0800

    relocated component

---
+++ js/ClientApp.jsx
@@ -0,0 +1,23 @@
+const ce = React.createElement
+
+const MyTitle = (props) => {
+  return (
+    ce('div', null, 
+      ce('h1', { style: {color: props.color} }, props.title)
+    )
+  )
+}
+
+const MyComponent = () => {
+  return ce('div', { id: 'my-component' }, 
+    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen'}),
+    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow'}),
+    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen'}),
+    ce(MyTitle, { title: 'Silicon Valleuy', color: 'peru'}),
+  )
+};
+
+ReactDOM.render(
+  ce(MyComponent), 
+  document.getElementById('app')
+)
