6a8ab4b77bf3f21b6d32264239bffb9b9ed293d7
thismax <max.g.jacobs@gmail.com>
2017-11-28 16:33:40 -0800

    prettier

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,21 +2,21 @@ const ce = React.createElement;
 
 const MyTitle = props => {
   return ce(
-    "div",
+    'div',
     null,
-    ce("h1", { style: { color: props.color } }, props.title)
+    ce('h1', { style: { color: props.color } }, props.title)
   );
 };
 
 const MyComponent = () => {
   return ce(
-    "div",
-    { id: "my-component" },
-    ce(MyTitle, { title: "Game of Thrones", color: "YellowGreen" }),
-    ce(MyTitle, { title: "Stranger Things", color: "GreenYellow" }),
-    ce(MyTitle, { title: "Rick and Morty", color: "LimeGreen" }),
-    ce(MyTitle, { title: "Silicon Valley", color: "peru" })
+    'div',
+    { id: 'my-component' },
+    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
+    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
+    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
   );
 };
 
-ReactDOM.render(ce(MyComponent), document.getElementById("app"));
+ReactDOM.render(ce(MyComponent), document.getElementById('app'));
