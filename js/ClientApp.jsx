cfb3fa584d538753c1782c2f16f248dadbb7fd99
thismax <max.g.jacobs@gmail.com>
2017-11-30 11:39:45 -0800

    added route, script

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
-
 import Landing from './Landing';
+import Search from './Search';
 
 const App = () => (
   <HashRouter>
     <div className="app">
       <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
     </div>
   </HashRouter>
 );
