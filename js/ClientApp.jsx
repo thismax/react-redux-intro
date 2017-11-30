68b4d8f4f849f65a75fc58f0eef2e3a6bfb37ff4
thismax <max.g.jacobs@gmail.com>
2017-11-29 18:02:11 -0800

    refactored main App component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const MyTitle = props => (
+  <div>
+    <h1 style={{ color: props.color }}>{props.title}</h1>
+  </div>
+);
 
-const MyTitle = props => ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+const MyComponent = () => (
+  <div id="my-component">
+    <MyTitle title="Game of Thrones" color="YellowGreen" />
+    <MyTitle title="Stranger Things" color="peru" />
+    <MyTitle title="House of Cards" color="GreenYellow" />
+    <MyTitle title="The Americans" color="cyan" />
+  </div>
+);
 
-const MyComponent = () =>
-  ce(
-    'div',
-    { id: 'my-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
-
-render(ce(MyComponent), document.getElementById('app'));
+render(<MyComponent />, document.getElementById('app'));
