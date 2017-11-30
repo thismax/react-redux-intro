84de12019f09e14c654db6851e62b8316921bc33
thismax <max.g.jacobs@gmail.com>
2017-11-30 15:21:23 -0800

    added show card

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import preload from '../data.json';
 
-const Search = () => <h1>lol this search</h1>;
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img src={`/public/img/posters/${show.poster}`} alt={`${show.title} Show Poster`} />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>{show.year}</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
 export default Search;
