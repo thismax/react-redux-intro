c20bee3a28cce1163a90d00fc0e4470d40dd8a1f
thismax <max.g.jacobs@gmail.com>
2017-11-29 17:26:51 -0800

    building webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, './public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+};
