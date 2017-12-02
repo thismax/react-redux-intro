c71e83079e2fdcae47c38a5a2d2a297443c3fd85
thismax <max.g.jacobs@gmail.com>
2017-12-01 18:42:05 -0800

    bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   context: __dirname,
   entry: [
     'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost8080',
+    'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
     './js/ClientApp.jsx'
   ],
