9d4cb9496c96a03ac496918d12cbedc603527bed
thismax <max.g.jacobs@gmail.com>
2017-12-02 15:10:00 -0800

    debugged HMR

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
 import App from './App';
 
-const renderApp = () => {
-  render(<App />, document.getElementById('app'));
+const renderApp = Component => {
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app')
+  );
 };
 
-renderApp();
+renderApp(App);
 
 if (module.hot) {
   module.hot.accept('./App.jsx', () => {
-    renderApp();
+    // eslint-disable-next-line global-require
+    const NextApp = require('./App').default;
+    renderApp(NextApp);
   });
 }
