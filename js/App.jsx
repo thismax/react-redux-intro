5bbbbc3b2fd43405a26103ca785809261ebd2a59
thismax <max.g.jacobs@gmail.com>
2017-12-07 18:48:23 -0800

    removed preload

--- js/App.jsx
+++ js/App.jsx
@@ -6,7 +6,6 @@ import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
-import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -15,11 +14,11 @@ const App = () => (
     <div className="app">
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
+        <Route path="/search" component={props => <Search shows={this.props.shows} {...props} />} />
         <Route
           path="/details/:id"
           component={(props: { match: Match }) => (
-            <Details show={preload.shows.find(show => props.match.params.id === show.imdbID)} {...props} />
+            <Details show={this.props.shows.find(show => props.match.params.id === show.imdbID)} {...props} />
           )}
         />
         <Route component={FourOhFour} />
