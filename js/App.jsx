1fc459631ef3944c1373e40cd4636f7c8985dd54
thismax <max.g.jacobs@gmail.com>
2017-12-07 18:25:50 -0800

    stubbed out Details component and route

--- js/App.jsx
+++ js/App.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -13,6 +14,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route path="/details/:id" component={Details} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
