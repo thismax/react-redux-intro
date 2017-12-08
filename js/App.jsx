4a7a8bed632015a4ba7f5b9b706cd783054cb747
thismax <max.g.jacobs@gmail.com>
2017-12-07 18:58:15 -0800

    reverted

--- js/App.jsx
+++ js/App.jsx
@@ -6,6 +6,7 @@ import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
+import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -14,11 +15,11 @@ const App = () => (
     <div className="app">
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/search" component={props => <Search shows={this.props.shows} {...props} />} />
+        <Route path="/search" component={props => <Search shows={preload.shows} {...props} />} />
         <Route
           path="/details/:id"
           component={(props: { match: Match }) => (
-            <Details show={this.props.shows.find(show => props.match.params.id === show.imdbID)} {...props} />
+            <Details show={preload.shows.find(show => props.match.params.id === show.imdbID)} {...props} />
           )}
         />
         <Route component={FourOhFour} />
