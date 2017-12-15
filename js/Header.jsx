f07edc63b70648724d27d853a104f315d3c5e5fe
thismax <max.g.jacobs@gmail.com>
2017-12-14 19:22:12 -0800

    added Header

---
+++ js/Header.jsx
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>
+      <Link to="/">
+        mvideo
+      </Link>
+    </h1>
+  </header>
+);
+
+export default Header;
