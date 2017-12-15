a1d06f43f72e142b217e4a36d4a50e1196db27fb
thismax <max.g.jacobs@gmail.com>
2017-12-14 19:51:46 -0800

    converted to stateful

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,29 +1,35 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
-const Details = (props: { show: Show }) => {
-  const { title, description, year, poster, trailer } = props.show;
-  return (
-    <div className="details">
-      <Header />
-      <section>
-        <h1>{title}</h1>
-        <h2>({year})</h2>
-        <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
-        <p>{description}</p>
-      </section>
-      <div>
-        <iframe
-          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
-          frameBorder="0"
-          allowFullScreen
-          title={`Trailer for ${title}`}
-        />
+class Details extends Component {
+  props: {
+    show: Show
+  };
+
+  render() {
+    const { title, description, year, poster, trailer } = this.props.show;
+    return (
+      <div className="details">
+        <Header />
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`}
+            frameBorder="0"
+            allowFullScreen
+            title={`Trailer for ${title}`}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Details;
