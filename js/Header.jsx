92f7be5c8e534f6053aae1faab4006258236bd36
thismax <max.g.jacobs@gmail.com>
2017-12-14 19:37:01 -0800

    relocated input

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,14 +3,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => (
-  <header>
-    <h1>
-      <Link to="/">
-        mvideo
-      </Link>
-    </h1>
-  </header>
-);
+const Header = (props: { showSearch?: boolean, handleSearchTermChange?: Function, searchTerm? string }) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = (
+      <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type="text" placeholder="Search" />
+    );
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search">
+          Back
+        </Link>
+      </h2>
+    );
+  }
+  return (
+    <header>
+      <h1>
+        <Link to="/">
+          mvideo
+        </Link>
+      </h1>
+      {utilSpace}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  showSearch: false,
+  handleSearchTermChange: function noop () {},
+  searchTerm: ''
+};
 
 export default Header;
