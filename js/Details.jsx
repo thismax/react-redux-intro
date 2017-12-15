f18bc18aa771b998273dace8913c6bc0f2a6a902
thismax <max.g.jacobs@gmail.com>
2017-12-14 20:07:22 -0800

    added rating api

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,21 +1,40 @@
 // @flow
 
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
+import Spinner from './Spinner';
 
 class Details extends Component {
+  state = {
+    apiData: { rating: '' }
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:3000/${this.props.show.imdbID}`).then((response: { data: { rating: string } }) => {
+      this.setState({ apiData: response.data });
+    });
+  }
+
   props: {
     show: Show
   };
 
   render() {
     const { title, description, year, poster, trailer } = this.props.show;
+    let ratingComponent;
+    if (this.state.apiData.rating) {
+      ratingComponent = <h3>{this.state.apiData.rating}</h3>;
+    } else {
+      ratingComponent = <Spinner />;
+    }
     return (
       <div className="details">
         <Header />
         <section>
           <h1>{title}</h1>
           <h2>({year})</h2>
+          {ratingComponent}
           <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
           <p>{description}</p>
         </section>
