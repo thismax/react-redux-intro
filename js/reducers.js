36500c64b94e2f0e47fac346c4dc3f7c7de5767e
thismax <max.g.jacobs@gmail.com>
2017-12-20 18:17:03 -0800

    created searchTerm action and reducer

--- js/reducers.js
+++ js/reducers.js
@@ -2,6 +2,10 @@ const DEFAULT_STATE = {
   searchTerm: ''
 };
 
+const setSearchTerm = (state, action) => {
+  return Object.assign({}, state, { searchTerm: action.payload });
+};
+
 const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     default:
