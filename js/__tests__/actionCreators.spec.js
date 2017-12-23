36fcd1767ebfe5223f986e095c1961dfbf13cc5e
thismax <max.g.jacobs@gmail.com>
2017-12-22 17:28:45 -0800

    added actionCreator snapshots

--- js/__tests__/actionCreators.spec.js
+++ js/__tests__/actionCreators.spec.js
@@ -1,3 +1,21 @@
 // @flow
 
 import { setSearchTerm, addAPIData } from '../actionCreators';
+
+test('setSearchTerm', () => {
+  expect(setSearchTerm('New York')).toMatchSnapshot();
+});
+
+test('addAPIData', () => {
+  expect(
+    addAPIData({
+      rating: '4.2',
+      title: 'Stranger Things',
+      year: '2016–',
+      description: 'When a young boy disappears, his mother, a police chief, and his friends must confront terrifying forces in order to get him back.',
+      poster: 'st.jpg',
+      imdbID: 'tt4574334',
+      trailer: '9Egf5U8xLo8'
+    })
+  ).toMatchSnapshot();
+});
